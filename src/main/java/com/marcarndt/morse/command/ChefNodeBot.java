b9d8052e8d848caee13c4726b257bb8254f8be76
Marc Arndt <marc@marcarndt.com>
2017-06-29 16:11:01 +0200

    findbugs

--- src/main/java/com/marcarndt/morse/command/ChefNodeBot.java
+++ src/main/java/com/marcarndt/morse/command/ChefNodeBot.java
@@ -6,6 +6,7 @@ import com.marcarndt.morse.dto.Node;
 import com.marcarndt.morse.service.ChefService;
 import com.marcarndt.morse.telegrambots.api.objects.Chat;
 import com.marcarndt.morse.telegrambots.api.objects.User;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -16,26 +17,49 @@ import javax.inject.Inject;
 @Stateless
 public class ChefNodeBot extends BaseCommand {
 
+  /**
+   * Logger.
+   */
   private static final Logger LOG = Logger.getLogger(ChefNodeBot.class.getName());
 
+  /**
+   * The Chef service.
+   */
   @Inject
-  ChefService chefService;
+  private transient ChefService chefService;
 
+  /**
+   * Trusted.
+   *
+   * @return Trusted
+   */
   @Override
   public String getRole() {
     return "Trusted";
   }
 
-  protected String performCommand(MorseBot morseBot, User user, Chat chat, String... arguments) {
+  /**
+   * Retreives basic information for a chef node.
+   * @param morseBot Morse bot
+   * @param user User
+   * @param chat Chat
+   * @param arguments first argument is the chef node
+   * @return Null
+   */
+  protected String performCommand(final MorseBot morseBot, final User user, final Chat chat, final String... arguments) {
     Node node = null;
     try {
       if (arguments.length != 1) {
         throw new MorseBotException("To use this command, please use /chef (node name)");
       }
 
-      LOG.info("Searching for node " + arguments[0]);
+      if (LOG.isLoggable(Level.INFO)) {
+        LOG.info("Searching for node " + arguments[0]);//NOPMD
+      }
       node = chefService.getNode(arguments[0]);
-      LOG.info("Found Node: " + node.toString());
+      if (LOG.isLoggable(Level.INFO)) {
+        LOG.info("Found Node: " + node.toString());//NOPMD
+      }
       morseBot
           .sendMessage(node.getName() + " - " + node.getEnvironment() + " - " + node.getPlatform(),
               chat.getId().toString());
@@ -45,11 +69,21 @@ public class ChefNodeBot extends BaseCommand {
     return null;
   }
 
+  /**
+   * chef
+   *
+   * @return chef
+   */
   @Override
   public String getCommandIdentifier() {
     return "chef";
   }
 
+  /**
+   * Description for the help file
+   *
+   * @return String
+   */
   @Override
   public String getDescription() {
     return "provides details on a chef node. to use run /chef (node name)";
